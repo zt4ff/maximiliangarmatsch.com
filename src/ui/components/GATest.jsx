8caa870a57f69f022c09c10eecaca69b31dde822
Maximilian Garmatsch <mail@maximiliangarmatsch.com>
2020-03-09 08:38:15 +0100

    feat: google analytics button tracking test

---
+++ src/ui/components/GATest.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function GATest () {
+
+    const myButton = () => {
+        console.log('### Landingpage test');
+        window.gtag('send', 'event', 'Amazon-Sports', 'Submit', 'Test Button', 'clicked');
+    };
+
+    return (
+        <button onClick={myButton}>
+            Test Button
+        </button>
+    );
+}
+
+export default GATest;
