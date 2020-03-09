c931c1de5ede9d3647bb5963cfca809a9c22cf53
Maximilian Garmatsch <mail@maximiliangarmatsch.com>
2020-03-09 08:40:19 +0100

    refactor: google analytics

--- src/ui/components/GATest.jsx
+++ src/ui/components/GATest.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
-function GATest () {
-
+function GATest() {
     const myButton = () => {
         console.log('### Landingpage test');
-        window.gtag('send', 'event', 'Amazon-Sports', 'Submit', 'Test Button', 'clicked');
+        window.gtag(
+            'send',
+            'event',
+            'Amazon-Sports',
+            'Submit',
+            'Test Button',
+            'clicked'
+        );
     };
 
-    return (
-        <button onClick={myButton}>
-            Test Button
-        </button>
-    );
+    return <button onClick={myButton}>Test Button</button>;
 }
 
 export default GATest;
