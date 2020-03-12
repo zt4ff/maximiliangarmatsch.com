89008b88d830876a21d9b7df72c84fa2dcc2bf45
Maximilian <mail@maximiliangarmatsch.com>
2020-03-12 08:31:34 +0100

    refactor: device provider set for every page individually

--- src/ui/pages/Impressum/Impressum.js
+++ src/ui/pages/Impressum/Impressum.js
@@ -3,20 +3,23 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { Box } from '../../components/Base/Base';
 import NavLinkHome from '../../templates/components/Navigation/NavLinkHome';
 import theme from '../../../config/theme';
+import DeviceProvider from '../../../helpers/DeviceProvider';
 import TextAdress from './components/TextAdress';
 import TextPrivacyPolicy from './components/TextPrivacyPolicy';
 
 export default function Impressum() {
     const isDesktop = useMediaQuery(`(min-width:${theme.breakpoints.md})`);
     return (
-        <Box lineHeight="1.3rem" mt="2rem" p="0 15px">
-            <TextAdress />
-            <TextPrivacyPolicy />
-            {!isDesktop && (
-                <Box textAlign="center" m="2rem">
-                    <NavLinkHome />
-                </Box>
-            )}
-        </Box>
+        <DeviceProvider>
+            <Box lineHeight="1.3rem" mt="2rem" p="0 15px">
+                <TextAdress />
+                <TextPrivacyPolicy />
+                {!isDesktop && (
+                    <Box textAlign="center" m="2rem">
+                        <NavLinkHome />
+                    </Box>
+                )}
+            </Box>
+        </DeviceProvider>
     );
 }
