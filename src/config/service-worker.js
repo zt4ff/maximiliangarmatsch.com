4a9caa7f4ebe892e0782ef4c84208d1060e07de2
ZaeemAhmed542 <zahmed.bscs18seecs@seecs.edu.pk>
2020-07-17 11:06:06 +0500

    added background sync

--- src/config/service-worker.js
+++ src/config/service-worker.js
@@ -5,3 +5,26 @@ self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
+const bgSyncPlugin = new workbox.backgroundSync.BackgroundSyncPlugin('queue', {
+    maxRetentionTime: 24 * 60 // Retry for max of 24 Hours
+});
+
+workbox.routing.registerRoute(
+    ({ url }) => {
+        return (url.origin.includes("yandex"));
+    },
+    new workbox.strategies.NetworkOnly({
+        plugins: [bgSyncPlugin]
+    }),
+    'POST'
+)
+
+workbox.routing.registerRoute(
+    ({ url }) => {
+        return (url.origin.includes("google-analytics/r/collect"));
+    },
+    new workbox.strategies.NetworkOnly({
+        plugins: [bgSyncPlugin]
+    }),
+    'GET'
+)
